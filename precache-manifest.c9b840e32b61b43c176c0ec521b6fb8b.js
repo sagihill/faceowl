584becb85ebe0f30e55c513b49e933f9edae37cc
sagihill <54205588+sagihill@users.noreply.github.com>
2019-12-05 17:13:21 +0200

    Updates

---
+++ precache-manifest.c9b840e32b61b43c176c0ec521b6fb8b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6457a25e06a943a64db5fc903fb02f1b",
+    "url": "/faceowl/index.html"
+  },
+  {
+    "revision": "633a4b01ebfdad350845",
+    "url": "/faceowl/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8180b676d3a5b332d9f9",
+    "url": "/faceowl/static/css/main.697260f4.chunk.css"
+  },
+  {
+    "revision": "633a4b01ebfdad350845",
+    "url": "/faceowl/static/js/2.551be3e7.chunk.js"
+  },
+  {
+    "revision": "8180b676d3a5b332d9f9",
+    "url": "/faceowl/static/js/main.5aea9ead.chunk.js"
+  },
+  {
+    "revision": "17ccc2229375e4243f29",
+    "url": "/faceowl/static/js/runtime-main.8957d45b.js"
+  },
+  {
+    "revision": "a152100e161f33640ac00355909ec37b",
+    "url": "/faceowl/static/media/Logo.a152100e.png"
+  },
+  {
+    "revision": "f67e64e4dd0e1397b07be00f4d952c5a",
+    "url": "/faceowl/static/media/Oswald-Regular.f67e64e4.ttf"
+  },
+  {
+    "revision": "80c5a8d1c5f93a81f9d234152db6b63d",
+    "url": "/faceowl/static/media/QuattrocentoSans-Regular.80c5a8d1.ttf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/faceowl/precache-manifest.c9b840e32b61b43c176c0ec521b6fb8b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/faceowl/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
